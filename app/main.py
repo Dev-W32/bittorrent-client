5c9d1ce64a044d0642c57a394a0f8a0ea4f6eebf
root <root@LAPTOP-00RIBFE2>
2024-09-25 13:02:23 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-# import bencodepy - available if you need it!
+import bencodepy 
 # import requests - available if you need it!
 
 # Examples:
@@ -9,14 +9,34 @@ import sys
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
 def decode_bencode(bencoded_value):
-    if chr(bencoded_value[0]).isdigit():
+    return bencodepy.decode(bencoded_value)
+    def decode_string(bencoded_value):
         first_colon_index = bencoded_value.find(b":")
         if first_colon_index == -1:
             raise ValueError("Invalid encoded value")
         return bencoded_value[int(first_colon_index) + 1 :]
+
+    def decode_integer(bencoded_value):
+        return int(bencoded_value[1:-1])
+    
+    if chr(bencoded_value[0]).isdigit():
+        return decode_string(bencoded_value)
+        
     
     elif chr(bencoded_value[0]) == "i" and chr(bencoded_value[-1]) == "e":
-        return int(bencoded_value[1:-1])
+        return decode_integer(bencoded_value)
+    
+    elif chr(bencoded_value[0]=="l"):
+        res = []
+        list_str = bencoded_value[1:-1]
+        for i in range(len(list_str)):
+            if chr(list_str[i]).isdigit():
+                string = decode_string(list_str[i:int(list_str[i])+2])
+                res.append(string)
+                i+=(len(string)+1)
+            
+         
+
 
     else:
         raise TypeError("Only strings are supported at the moment")
