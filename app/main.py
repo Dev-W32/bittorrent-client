1a9179b812af2ffd56f26ec3266253b0ed81b7d0
root <root@LAPTOP-00RIBFE2>
2024-09-25 12:29:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,9 +13,14 @@ def decode_bencode(bencoded_value):
         first_colon_index = bencoded_value.find(b":")
         if first_colon_index == -1:
             raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
+        return bencoded_value[int(first_colon_index) + 1 :]
+    
+    elif chr(bencoded_value[0]) == "i" and chr(bencoded_value[-1]) == "e":
+        return int(bencoded_value[1:-1])
+
     else:
-        raise NotImplementedError("Only strings are supported at the moment")
+        raise TypeError("Only strings are supported at the moment")
+
 
 
 def main():
@@ -26,11 +31,14 @@ def main():
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
 
+        
+          
         # json.dumps() can't handle bytes, but bencoded "strings" need to be
         # bytestrings since they might contain non utf-8 characters.
         #
         # Let's convert them to strings for printing to the console.
         def bytes_to_str(data):
+            
             if isinstance(data, bytes):
                 return data.decode()
 
